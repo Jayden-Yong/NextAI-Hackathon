e73371ea0fd42997bbae498dd8cb87d830e9061a
Jayden Yong <jaydenyong28@gmail.com>
2025-02-10 10:31:49 +0000

    Updated .gitignore to properly ignore config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-api_key = 'sk-799e9b5dcd3947c48390420dcb79e1a6'
-# (DICT) MySQL configuration
-DB_CONFIG = {
-    'host':'localhost',
-    'user':'root',
-    'password':'755788',
-    'database':'hybridhub'
-}
