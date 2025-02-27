620d6ee54eb045f884ecd1c051a7d5f50fe869b3
Jayden Yong <jaydenyong28@gmail.com>
2025-02-27 18:01:36 +0800

    Bug fixes and update for database

--- hybridhub.sql
+++ hybridhub.sql
@@ -26,16 +26,26 @@ DROP TABLE IF EXISTS `accounts`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accounts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `password_hash` varchar(255) NOT NULL,
   `employeeID` varchar(6) NOT NULL,
   `access` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `employeeID_UNIQUE` (`employeeID`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   CONSTRAINT `employeeID-foreign` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'admin@test.com','$2y$10$8yOLa74BG81ydpdpRM/3wuCWur8x.r3ELBXGVcG3ikSMBULxAKZ2u','A1',0),(2,'shaun@test.com','$2y$10$8yOLa74BG81ydpdpRM/3wuCWur8x.r3ELBXGVcG3ikSMBULxAKZ2u','E00001',1),(3,'jaydenyong28@gmail.com','$2y$10$8yOLa74BG81ydpdpRM/3wuCWur8x.r3ELBXGVcG3ikSMBULxAKZ2u','A2',0);
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `booking`
@@ -46,16 +56,57 @@ DROP TABLE IF EXISTS `booking`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `booking` (
   `bookingID` int NOT NULL AUTO_INCREMENT,
-  `employeeID` varchar(6) NOT NULL,
-  `deskID` varchar(6) NOT NULL,
-  `date` datetime NOT NULL,
+  `employeeID` varchar(6) DEFAULT NULL,
+  `deskID` varchar(6) DEFAULT NULL,
+  `date` date NOT NULL,
   PRIMARY KEY (`bookingID`),
   KEY `employeeID_idx` (`employeeID`),
   KEY `deskID_idx` (`deskID`),
-  CONSTRAINT `deskID` FOREIGN KEY (`deskID`) REFERENCES `desk` (`deskID`) ON UPDATE CASCADE,
-  CONSTRAINT `employeeID` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `deskID` FOREIGN KEY (`deskID`) REFERENCES `desk` (`deskID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `employeeID` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+INSERT INTO `booking` VALUES (1,'E00001',NULL,'2025-02-26');
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookmeeting`
+--
+
+DROP TABLE IF EXISTS `bookmeeting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookmeeting` (
+  `meetingID` int NOT NULL AUTO_INCREMENT,
+  `employeeID` varchar(6) DEFAULT NULL,
+  `deskID` varchar(6) DEFAULT NULL,
+  `startTime` datetime NOT NULL,
+  `endTime` datetime NOT NULL,
+  PRIMARY KEY (`meetingID`),
+  KEY `deskID_idx` (`deskID`),
+  KEY `employeeID_idx` (`employeeID`),
+  CONSTRAINT `meetingDeskID` FOREIGN KEY (`deskID`) REFERENCES `desk` (`deskID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `meetingEmployeeID` FOREIGN KEY (`employeeID`) REFERENCES `employee` (`employeeID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookmeeting`
+--
+
+LOCK TABLES `bookmeeting` WRITE;
+/*!40000 ALTER TABLE `bookmeeting` DISABLE KEYS */;
+INSERT INTO `bookmeeting` VALUES (1,'A2',NULL,'2025-02-24 12:00:00','2025-02-24 14:00:00'),(2,'A1',NULL,'2025-02-26 21:00:00','2025-02-27 02:00:00'),(3,'E00001',NULL,'2025-02-28 08:00:00','2025-02-28 10:00:00');
+/*!40000 ALTER TABLE `bookmeeting` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `department`
@@ -72,6 +123,16 @@ CREATE TABLE `department` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('D00','Directors'),('D01','Finance');
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `desk`
 --
 
@@ -85,6 +146,16 @@ CREATE TABLE `desk` (
   PRIMARY KEY (`deskID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `desk`
+--
+
+LOCK TABLES `desk` WRITE;
+/*!40000 ALTER TABLE `desk` DISABLE KEYS */;
+INSERT INTO `desk` VALUES ('D10',418,206),('D40',601,139),('D41',419,142),('D42',419,277),('D43',326,206),('D5',603,280),('D51',141,278),('D57',140,346),('D63',600,75),('D64',326,140),('D66',603,348),('D72',142,76),('D79',139,208),('D85',141,141),('D89',600,209),('D96',325,278),('M1',787,388),('M33',786,231),('M47',1047,386),('M71',1048,68),('M77',784,71),('M88',279,423),('M91',425,422),('M92',1048,234);
+/*!40000 ALTER TABLE `desk` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `employee`
@@ -104,6 +175,16 @@ CREATE TABLE `employee` (
   CONSTRAINT `departmentID` FOREIGN KEY (`departmentID`) REFERENCES `department` (`departmentID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES ('A1','Jayden','5','D00'),('A2','Jayden','4','D00'),('E00001','Shaun','1','D01');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -114,4 +195,4 @@ CREATE TABLE `employee` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-22 11:21:04
+-- Dump completed on 2025-02-27 17:58:35
